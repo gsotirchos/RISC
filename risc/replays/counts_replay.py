4cb4c42dbd7e2c89f2de30abc2ec21804a3e881b
gsotirchos <gsotirchos@student.tudelft.nl>
2025-01-17 23:41:44 +0100

    Add limited counts option

--- risc/replays/counts_replay.py
+++ risc/replays/counts_replay.py
@@ -4,8 +4,9 @@ from replays.circular_replay import CircularReplayBuffer
 
 
 class CountsReplayBuffer(CircularReplayBuffer):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, limited_counts=True, **kwargs):
         super().__init__(*args, **kwargs)
+        self.limited_counts = limited_counts
         self.counts = {}
         for key in self._specs:
             self.counts[key] = {}
@@ -18,11 +19,12 @@ class CountsReplayBuffer(CircularReplayBuffer):
 
     def _update_counts(self, key, **transition):
         """ If a transition will be overwritten decrement its item's count and if it has no counts left remove its entry. Then increment the count for the new item. """
-        # overwritten_item = self._to_tuple(self._storage[key][self._cursor])
-        # if not (np.array(overwritten_item) == 0).all():
-        #     self.counts[key][overwritten_item] -= 1
-        #     if self.counts[key][overwritten_item] == 0:
-        #         del self.counts[key][overwritten_item]
+        if self.limited_counts:
+            overwritten_item = self._to_tuple(self._storage[key][self._cursor])
+            if not (np.array(overwritten_item) == 0).all():
+                self.counts[key][overwritten_item] -= 1
+                if self.counts[key][overwritten_item] == 0:
+                    del self.counts[key][overwritten_item]
         new_item = self._to_tuple(transition[key])
         if not (np.array(new_item) == 0).all():
             self.counts[key][new_item] = self.counts[key].get(new_item, 0) + 1
