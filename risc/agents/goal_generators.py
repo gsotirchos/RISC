2eadba36c89df0355993ac610710926c85790425
gsotirchos <gsotirchos@student.tudelft.nl>
2025-04-26 12:31:40 +0200

    Fix buggy timeout-switching behavior

--- risc/agents/goal_generators.py
+++ risc/agents/goal_generators.py
@@ -11,7 +11,7 @@ from hive.utils.loggers import Logger, NullLogger
 from hive.utils.registry import Registrable
 from hive.utils.schedule import PeriodicSchedule
 from hive.utils.utils import seeder
-#from replays.counts_replay import HashableKeyDict
+from replays.counts_replay import HashableKeyDict
 from scipy.special import expit as sigmoid
 from scipy.special import softmax
 
@@ -165,10 +165,12 @@ class OmniGoalGenerator(GoalGenerator):
         counts = agent._replay_buffer.action_counts
         if len(counts) == 0:
             return None, None
-        if (self._max_visitations == 0
-                or self._max_visitations < min(counts.values())
-                or self._max_visitations >= max(counts.values())):
-            def satisfies_condition(count): return True
+        if (
+            self._max_visitations == 0
+            or self._max_visitations < min(counts.values())
+            or self._max_visitations >= max(counts.values())
+        ):
+            def satisfies_condition(_): return True
         else:
             def satisfies_condition(count): return count <= self._max_visitations
         frontier_state_actions = [
@@ -177,7 +179,7 @@ class OmniGoalGenerator(GoalGenerator):
         frontier_states, frontier_actions = zip(*frontier_state_actions)
         return np.array(frontier_states), np.array(frontier_actions)
 
-    def _novelty(self, agent, states, actions = None):
+    def _novelty(self, states, actions, agent):
         if states.ndim == len(agent._observation_space.shape):
             states = np.expand_dims(states, axis=0)
         if actions is not None:
@@ -186,7 +188,7 @@ class OmniGoalGenerator(GoalGenerator):
         novelties = np.array([1 / (counts[state] + epsilon) for state in states])
         return novelties
 
-    def _confidence(self, agent, observations, goals):
+    def _confidence(self, observations, goals, agent):
         if self._oracle:
             agent = agent._oracle
         if self._use_success_prob:
@@ -214,25 +216,25 @@ class OmniGoalGenerator(GoalGenerator):
         cost_to_go = np.zeros(len(frontier_states))
         if self._weights[0] != 0:
             # TODO: use a distribution
-            novelty_cost = sigmoid(self._novelty(agent, frontier_states, frontier_actions))
-            #novelty_cost = sigmoid(zscore(1 / (self._novelty(agent, frontier_states, frontier_actions) + epsilon)))
+            novelty_cost = sigmoid(self._novelty(frontier_states, frontier_actions, agent))
+            #novelty_cost = sigmoid(zscore(1 / (self._novelty(frontier_states, frontier_actions, agent) + epsilon)))
         if self._weights[1] != 0:
             cost_to_reach = self._cost(
-                agent,
                 observation,
-                frontier_states[:, None, 0]
+                frontier_states[:, None, 0],
+                agent
             )
         if self._weights[2] != 0:
             cost_to_come = self._cost(
-                agent,
                 np.concatenate([initial_state, observation[1][None, ...]]),
-                frontier_states[:, None, 0]
+                frontier_states[:, None, 0],
+                agent
             )
         if self._weights[3] != 0:
             cost_to_go = self._cost(
-                agent,
                 frontier_states,
-                goal_state
+                goal_state,
+                agent
             )
         priority = softmin(
             novelty_cost ** self._weights[0]
@@ -249,8 +251,8 @@ class OmniGoalGenerator(GoalGenerator):
         self._dbg_print(f"observation: {self._dbg_format(observation[0])}")
         self._dbg_print(f"curent direction: {agent_traj_state.current_direction}")
         agent, initial_state, goal_state = self._get_agent_init_goal_states(agent_traj_state)
-        curret_direction = agent_traj_state.current_direction.removeprefix("teleport_")
-        if curret_direction == "lateral":
+        current_direction = agent_traj_state.current_direction.removeprefix("teleport_")
+        if current_direction == "lateral":
             frontier_states, frontier_actions = self._get_frontier(agent)
             self._dbg_print(f"frontier state-actions: {self._dbg_format(frontier_states[:, 0], frontier_actions)}")
             if frontier_states is None:
@@ -277,8 +279,8 @@ class OmniGoalGenerator(GoalGenerator):
             )
             goal_idx = self._rng.choice(len(priority), p=priority)  # np.argmin(priority)
             goal = frontier_states[goal_idx, 0][None, ...], frontier_actions[goal_idx]
-            #self._dbg_print(f"visitations: {(1 / self._novelty(agent, frontier_states, frontier_actions)).astype(int)}", "   ")
-            #self._dbg_print(f"stdzed vis.: {zscore(1 / self._novelty(agent, frontier_states, frontier_actions))}", "   ")
+            #self._dbg_print(f"visitations: {(1 / self._novelty(frontier_states, frontier_actions, agent)).astype(int)}", "   ")
+            #self._dbg_print(f"stdzed vis.: {zscore(1 / self._novelty(frontier_states, frontier_actions, agent))}", "   ")
             self._dbg_print(f"novelty costs: {novelty_cost}", "   ")
             self._dbg_print(f"priority: {np.round(priority, 3)}", "   ")
             if self._log_schedule.update() and not isinstance(self._logger, NullLogger):
@@ -303,7 +305,7 @@ class OmniGoalGenerator(GoalGenerator):
                     },
                     f"{agent._id.removesuffix('_agent')}_goal_generator",
                 )
-        else:  # curret_direction != "lateral"
+        else:  # current_direction != "lateral"
             goal = goal_state, None
         self._dbg_print(f"goal state: {self._dbg_format(goal[0])}", "   ")
         self._dbg_print(f"goal action: {goal[1]}", "   ")
@@ -366,8 +368,9 @@ class TimeoutGoalSwitcher(GoalSwitcher):
         logger: Logger,
         log_frequency: int = 25,
         threshold: float = 0.75,
+        novel_threshold: int = 10,
+        familiar_threshold: int = 1,
         window_size: int = 5,
-        #oracle: bool = False,
         **kwargs,
     ):
         self._forward_agent = forward_agent
@@ -376,39 +379,64 @@ class TimeoutGoalSwitcher(GoalSwitcher):
         self._logger = logger
         self._rng = np.random.default_rng(seeder.get_new_seed("goal_switcher"))
         self._log_schedule = PeriodicSchedule(False, True, log_frequency)
-        #self._oracle = oracle
         self._threshold = threshold
         self._window_size = window_size
         self._window = deque(np.zeros(window_size), maxlen=window_size)
         self._window_avg = 0
+        #self._novel_threshold = novel_threshold
+        #self._familiar_threshold = familiar_threshold
+        #self._familiar_obs_count = 0
+        self._novel_observations = HashableKeyDict()
+
+
+    def _is_novel(self, observation, agent):
+        observation_counts = agent._replay_buffer.state_counts.get(observation, 0)
+        return observation_counts == 0 or observation in self._novel_observations
 
     def should_switch(self, update_info, agent_traj_state):
         agent = self._forward_agent if agent_traj_state.forward else self._backward_agent
         success_prob = 0.0
-        # TODO: oracle
-        #success_prob = agent.compute_success_prob(  # = cost-to-go
-        #    update_info.observation["observation"],
-        #    agent_traj_state.current_goal
-        #)
-        #initial_state = self._initial_states[self._rng.integers(len(self._initial_states))]
-        #cost_to_come = agent.compute_success_prob(
-        #    np.concatenate([initial_state, observation["observation"][1][None, ...]]),
-        #    observation["observation"][:, None, 0]
-        #)
-        #... = agent_traj_state.phase_steps / cost_to_come  # ???
-        if agent_traj_state.current_direction == "forward":
+        current_direction = agent_traj_state.current_direction.removeprefix("teleport_")
+        if current_direction != "lateral":
             return False, success_prob
-        if agent_traj_state.phase_steps == 0:
+        if agent_traj_state.phase_steps == 1:
             self._window = deque(np.zeros(self._window_size), maxlen=self._window_size)
             self._window_avg = 0
-        is_new_obs = agent._replay_buffer.state_counts.get(
-            update_info.next_observation["observation"],
-            0
-        ) <= 1
-        #breakpoint()
-        self._window_avg += (is_new_obs - self._window[0]) / self._window_size
-        self._window.append(is_new_obs)
+            self._novel_observations = HashableKeyDict()
+        next_observation = update_info.next_observation["observation"]
+        obs_is_new = self._is_novel(next_observation, agent)
+        if obs_is_new:
+            self._novel_observations[next_observation] = 1
+        self._window_avg += (obs_is_new - self._window[0]) / self._window_size
+        self._window.append(obs_is_new)
         return self._window_avg >= self._threshold, success_prob
+        #if agent_traj_state.phase_steps == 1:
+        #    #print("=== Resetting goal switcher ===")
+        #    self._familiar_obs_count = 0
+        #    self._novel_observations = HashableKeyDict()
+        #next_observation = update_info.next_observation["observation"]
+        #if self._is_novel(next_observation, agent):
+        #    #print("=== Novel obs ===")
+        #    #breakpoint()
+        #    self._novel_observations[next_observation] = 1
+        #if len(self._novel_observations) < self._novel_threshold:
+        #    #print("=== Novel obs: Continuing ===")
+        #    #breakpoint()
+        #    return False, success_prob
+        #if len(self._novel_observations) > self._novel_threshold:
+        #    #print("=== Novel obs: Truncating ===")
+        #    #breakpoint()
+        #    return True, success_prob
+        #if not self._is_novel(update_info.next_observation, agent):
+        #    #print("=== Familiar obs ===")
+        #    self._familiar_obs_count += 1
+        #if self._familiar_obs_count >= self._familiar_threshold:
+        #    #print("=== Familiar obs: Resetting goal switcher ===")
+        #    self._novel_observations = HashableKeyDict()
+        #    self._familiar_obs_count = 0
+        #print("=== Familiar obs: Continuing ===")
+        ##breakpoint()
+        #return False, success_prob
 
 
 class SuccessProbabilityGoalSwitcher(GoalSwitcher):
@@ -465,7 +493,7 @@ class SuccessProbabilityGoalSwitcher(GoalSwitcher):
         if not agent_traj_state.goal_switching_state:
             return False, success_prob
         switching_prob = success_prob*(1 - self._conservative_factor**agent_traj_state.phase_steps)
-        should_switch =  self._rng.random() < switching_prob
+        should_switch = self._rng.random() < switching_prob
         prefix = agent_traj_state.current_direction
         if self._log_schedule.update() and not isinstance(self._logger, NullLogger):
             self._logger.log_metrics(
