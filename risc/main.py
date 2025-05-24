4ed8670c2cc8f843521c43fba9927c863c8a33ff
gsotirchos <gsotirchos@student.tudelft.nl>
2025-05-24 17:58:06 +0200

    Revert for testing

--- risc/main.py
+++ risc/main.py
@@ -6,9 +6,6 @@ import envs
 import replays
 import runners
 import wandb_logger
-import hive.envs
-from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing
-from hive.envs.gym.gym_wrappers import PermuteImageWrapper
 from hive.main import main
 
 logging.basicConfig(
