45d129ce7b24b6da7989ce0f094151bb5554846f
gsotirchos <gsotirchos@student.tudelft.nl>
2025-09-08 22:41:04 +0200

    Fix sub-goal placement & removal; add triangles to Slide tiles

--- risc/envs/minigrid_envs.py
+++ risc/envs/minigrid_envs.py
@@ -10,10 +10,95 @@ from minigrid.core.constants import DIR_TO_VEC, COLORS
 from minigrid.core.grid import Grid
 from minigrid.core.mission import MissionSpace
 from minigrid.core.world_object import Goal, WorldObj, Floor
+from minigrid.utils.rendering import fill_coords, point_in_rect, point_in_triangle
 from minigrid.core.roomgrid import RoomGrid as _RoomGrid
 from minigrid.envs.empty import EmptyEnv as _EmptyEnv
 # from minigrid.envs.fourrooms import FourRoomsEnv as _FourRoomsEnv
-from minigrid.utils.rendering import fill_coords, point_in_rect
+
+
+class Floor(WorldObj):
+    def __init__(self, color: str = "grey"):
+        super().__init__("floor", color)
+
+    def can_overlap(self):
+        return True
+
+    def render(self, img):
+        fill_coords(img, point_in_rect(0.031, 1, 0.031, 1), (0, 0, 0))
+
+
+class Slide(WorldObj):
+    def __init__(self, action):
+        self.action = action
+        super().__init__("wall", "purple")
+
+    def can_overlap(self):
+        return True
+
+    def can_pickup(self):
+        return False
+
+    def can_contain(self):
+        return False
+
+    def see_behind(self):
+        return True
+
+    # def render(self, img):
+    #     fill_coords(img, point_in_rect(0.031, 1, 0.031, 1), COLORS[self.color])
+
+    def render(self, img):
+        fill_coords(img, point_in_rect(0, 1, 0, 1), COLORS[self.color])
+
+        tri1_pts = np.array([
+            [0.2, 0.5],
+            [0.5, 0.2],
+            [0.5, 0.8],
+        ])
+        tri2_pts = np.array([
+            [0.5, 0.5],
+            [0.8, 0.2],
+            [0.8, 0.8],
+        ])
+
+        rotations = {
+            0: [[ 1,  0], [ 0,  1]],
+            1: [[ 0, -1], [ 1,  0]],
+            2: [[-1,  0], [ 0, -1]],
+            3: [[ 0,  1], [-1,  0]],
+        }
+
+        rot_mat = rotations[self.action]
+        breakpoint()
+
+        # Apply the rotation to the vertices and add 0.5 to center them on the tile
+        center_offset = np.array([0.5, 0.5])
+        tri1_rotated = np.dot(tri1_pts - center_offset, rot_mat) + center_offset
+        tri2_rotated = np.dot(tri2_pts - center_offset, rot_mat) + center_offset
+
+        # Draw the triangles
+        fill_coords(img, point_in_triangle(*tri1_rotated), (0, 0, 0))
+        fill_coords(img, point_in_triangle(*tri2_rotated), (0, 0, 0))
+
+
+class Subgoal(WorldObj):
+    def __init__(self):
+        super().__init__("floor", "blue")
+
+    def can_overlap(self):
+        return True
+
+    def can_pickup(self):
+        return False
+
+    def can_contain(self):
+        return False
+
+    def see_behind(self):
+        return True
+
+    def render(self, img):
+        fill_coords(img, point_in_rect(0, 1, 0, 1), COLORS[self.color])
 
 
 class MiniGridEnv(minigrid.minigrid_env.MiniGridEnv):
@@ -27,55 +112,6 @@ class MiniGridEnv(minigrid.minigrid_env.MiniGridEnv):
         down = 1
         left = 2
         up = 3
-
-    class Slide(WorldObj):
-        def __init__(self, action):
-            self.action = action
-            super().__init__("wall", "purple")
-
-        def can_overlap(self) -> bool:
-            """Can the agent overlap with this?"""
-            return True
-
-        def can_pickup(self) -> bool:
-            """Can the agent pick this up?"""
-            return False
-
-        def can_contain(self) -> bool:
-            """Can this contain another object?"""
-            return False
-
-        def see_behind(self) -> bool:
-            """Can the agent see behind this object?"""
-            return True
-
-        def render(self, img):
-            """Draw this object with the given renderer"""
-            fill_coords(img, point_in_rect(0, 1, 0, 1), COLORS[self.color])
-
-    class Subgoal(WorldObj):
-        def __init__(self):
-            super().__init__("floor", "blue")
-
-        def can_overlap(self) -> bool:
-            """Can the agent overlap with this?"""
-            return True
-
-        def can_pickup(self) -> bool:
-            """Can the agent pick this up?"""
-            return False
-
-        def can_contain(self) -> bool:
-            """Can this contain another object?"""
-            return False
-
-        def see_behind(self) -> bool:
-            """Can the agent see behind this object?"""
-            return True
-
-        def render(self, img):
-            """Draw this object with the given renderer"""
-            fill_coords(img, point_in_rect(0, 1, 0, 1), COLORS[self.color])
 
     def __init__(
         self,
@@ -234,7 +270,7 @@ class MiniGridEnv(minigrid.minigrid_env.MiniGridEnv):
             self.put_obj(Goal(), *pos)
 
     def place_subgoal(self, pos):
-        self.put_obj(self.Subgoal(), *pos)
+        self.put_obj(Subgoal(), *pos)
 
     def place_floor(self, pos):
         self.put_obj(Floor(), *pos)
@@ -508,8 +544,8 @@ class HallwayEnv(MiniGridEnv):
         for i in range(self._num_hallways):
             offset = int(np.ceil(i / 2) * np.ceil(self.height / 4)) * (- 1 + int(i == 0)) ** i
             for x in range(self._hallway_start_x, self._hallway_end_x):
-                self.put_obj(self.Slide(self.actions.up), x, self._hallway_y + offset - 1)
-                self.put_obj(self.Slide(self.actions.down), x, self._hallway_y + offset + 1)
+                self.put_obj(Slide(self.actions.up), x, self._hallway_y + offset - 1)
+                self.put_obj(Slide(self.actions.down), x, self._hallway_y + offset + 1)
             try:
                 self.grid.vert_wall(self._hallway_end_x + 1, self._hallway_y + offset - 1, 3)
             except Exception:
